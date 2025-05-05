dc328d949b7ab078b06abc495900b20e1070d98a
Noma <philliswadin@gmail.com>
2025-05-05 11:32:41 +0200

    created a counter function to count features

---
+++ features.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int count(int x){
+    int counter = 0;
+    for(int i = 0; i < x; i++){
+        counter++;
+    }
+    return counter;
+}
+
+int main(){
+    int num = 10;
+
+
+}
